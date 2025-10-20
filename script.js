5ae80e22411ef4794d7402f377886bf91c4885ca
RayenTroudi <rayentroudi00@gmail.com>
2025-10-20 15:32:21 +0100

    kl

--- script.js
+++ script.js
@@ -1268,13 +1268,9 @@ window.testAdminEmail = testAdminEmail;
 
 // Gallery Lightbox Functionality
 const galleryImages = [
-	'gallery-1.jpg',
-	'gallery-2.jpg', 
 	'gallery-3.jpg',
 	'gallery-4.jpg',
 	'gallery-5.jpg',
-	'gallery-6.jpg',
-	'gallery-7.jpg',
 	'gallery-8.jpg'
 ];
 
